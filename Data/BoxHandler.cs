92eb2aebeef84994b4d6b67aa10d45e31273c8e2
ChandlerMcCook <chandler.mccook@gmail.com>
2025-02-01 03:24:46 -0600

    Setting up to receive data from backend

--- Data/BoxHandler.cs
+++ Data/BoxHandler.cs
@@ -1,12 +1,5 @@
 namespace faceoff.Data {
-    // This class exists to provide functions to send the images to the backend
-    public class ImageHandler {
-        public void SaveBinary(byte[] data, String path) {
-            using var writer = new BinaryWriter(File.OpenWrite(path));
-            writer.Write(data);
-        }
-
-
+    public class BoxHandler {
         public class Box {
             public int X = 50;
             public int Y = 50;
